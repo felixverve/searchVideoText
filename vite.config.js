0d11fbc1e13c00a672c013e062143e2ca837ff8c
FelixTian <cs546175240@126.com>
2025-12-06 23:40:33 +0800

    vite 配置

--- vite.config.js
+++ vite.config.js
@@ -2,4 +2,8 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 export default defineConfig({
     plugins: [react()],
+    server: {
+        host: '0.0.0.0',
+        allowedHosts: true // ⚠️ 允许任意主机，生产禁用
+    }
 });
